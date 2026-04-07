87a1e7450b339f507de47e9ed09519209c064b9b
jorgersn <jorgersn04@gmail.com>
2026-04-07 20:44:27 -0300

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,20 @@
+-- Banco de Dados
+USE BD240226114;
 
+-- Tabela de funcionários
+CREATE TABLE funcionarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cargo VARCHAR(50)
+);
+
+-- Tabela de processos
+CREATE TABLE processos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(200) NOT NULL,
+    urgencia INT,
+    id_funcionario INT,
+
+    FOREIGN KEY (id_funcionario)
+        REFERENCES funcionarios(id)
+);
