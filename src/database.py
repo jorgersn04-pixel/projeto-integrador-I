56782a8f5a2f2df7a789c9fd46d4f681218dec81
jorgersn <jorgersn04@gmail.com>
2026-04-08 16:26:30 -0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,1 +1,28 @@
+import mysql.connector
 
+
+class Database:
+    def __init__(self):
+        self.host = "localhost"
+        self.user = "root"
+        self.password = "SUA_SENHA"
+        self.database = "helpdesk_db"
+        self.conexao = None
+
+    def conectar(self):
+        try:
+            self.conexao = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database,
+                charset="utf8"
+            )
+            return self.conexao
+        except mysql.connector.Error as erro:
+            print(f"Erro ao conectar ao banco: {erro}")
+            return None
+
+    def desconectar(self):
+        if self.conexao and self.conexao.is_connected():
+            self.conexao.close()
