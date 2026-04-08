faea5166f90c36603544720dc1f3003d2465bdd9
jorgersn <jorgersn04@gmail.com>
2026-04-08 16:23:09 -0300

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,21 @@
+USE BD240226114;
 
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20),
+    tipo ENUM('cliente', 'tecnico') NOT NULL
+);
+
+CREATE TABLE chamados (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT NOT NULL,
+    id_tecnico INT NULL,
+    titulo VARCHAR(150) NOT NULL,
+    descricao TEXT NOT NULL,
+    status ENUM('Aberto', 'Em andamento', 'Fechado') DEFAULT 'Aberto',
+    data_abertura DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_cliente) REFERENCES usuarios(id),
+    FOREIGN KEY (id_tecnico) REFERENCES usuarios(id)
+);
